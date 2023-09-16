92da3296803d1de5b52a9a09983b59cb7048f24c
Yarb Roberts <yarb@pornhub.gov>
2023-09-16 03:52:24 +0200

    initial project. added empty home screen

---
+++ lib/homescreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
